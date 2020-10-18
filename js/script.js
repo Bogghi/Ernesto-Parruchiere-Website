f131f3a2aad54876796b1d9b9ded138a2e25c2ff
Matteo Borghi <matteosimoneborghi@gmail.com>
2020-10-18 20:34:34 +0200

    added resopnsivnes to the header

--- js/script.js
+++ js/script.js
@@ -19,3 +19,51 @@ anime.timeline().add({
         offset: '-=875',
         delay: (el, i, l) => 80 * (l - i)
     });
+
+function activeLiChange(){
+    let activeLi = document.querySelector("li.active");
+    if(this != activeLi){
+        this.classList.add("active");
+        activeLi.classList.remove("active");
+    }
+}
+
+const ul = document.querySelectorAll("nav ul li");
+
+ul.forEach(li => {
+    li.addEventListener("click", activeLiChange);
+});
+
+function showHbMenu (showVar) { 
+    let menu = document.querySelector(".menu");
+    let displayStatus = menu.style.display;
+
+    if(!showVar){
+        anime.timeline().add({
+            targets: '.menu',
+            opacity: [0,1],
+            height: "320px",
+            easing: "linear",
+            duration: 600,
+            delay: (el, i) => 70 * (i+1)
+        })
+        return true;
+    }else {
+        anime.timeline().add({
+            targets: ".menu",
+            opacity: 0,
+            height: "0px",
+            duration: 1000,
+            easing: "easeOutExpo"
+            // delay: 400
+          });
+        return false;
+    }
+}
+
+const hbMenu = document.querySelector(".mobile-menu");
+var isHidden = false;
+
+hbMenu.addEventListener("click",function(){
+    isHidden = showHbMenu(isHidden)
+});
