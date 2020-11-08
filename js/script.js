819e1a55bc342ef5231542166d588a37e9c28281
Matteo Borghi <matteosimoneborghi@gmail.com>
2020-11-08 16:31:20 +0100

    moved scroll code into scrip.js and completed the feature

--- js/script.js
+++ js/script.js
@@ -1,3 +1,100 @@
+/* constructor inizialize the attributes
+@currentPosition depending on the position where the page gets loaded we get a pointer where we are at
+@index array containing the amout of pixel from the document top
+*/
+let currentPosition = document.documentElement.scrollTop;
+let index = scrollPointArray("#scroll-point");
+let dest = 0;
+let dir = '';
+
+/* array builder for the index
+@scrollPoints nodeList containing the pointer in the document
+@scrollPointsIndex array containing the offest from document top for each pointer
+this fucntion return an oject containing:
+i: name of the destination link
+topDist: distance from the top of the document of the link
+its easy access thank to the array organization that find itself in the html
+*/
+function scrollPointArray(selector){
+    let scrollPoints = document.querySelectorAll(selector);
+    let scrollPointsIndex = [];
+    
+    for(let i = 0; i < scrollPoints.length; i++){
+        let point = scrollPoints[i];
+        point.setAttribute('pId',i);
+        let crtIndex = {
+            i: point.getAttribute('name'),
+            topDist: getDistanceFromTop(point)
+        };
+        scrollPointsIndex.push(crtIndex);
+    }
+    return scrollPointsIndex;
+}
+
+/* function to calculate the distance from the top of the document of the @element
+@element element wher the distance get calculated
+@yPost var containing the final distance ( starts at -100 because the navbar is 100px )
+*/
+function getDistanceFromTop(element) {
+    let yPos = -100;
+
+    while(element) {
+        yPos += (element.offsetTop);
+        element = element.offsetParent;
+    }
+
+    return yPos;
+}
+/* update the value of the attribute currentPosition
+this method can be attached to a event listener on the scroll event to keep track of where we are in the page
+*/
+function updateCurrentPosotion(){
+    currentPosition = document.documentElement.scrollTop;
+}
+
+/* select and manage the scroll direction calling the pageScroll method
+
+@id is the index id to get the element to scroll to
+@dest populate the dest variable to indicate where we have to stop
+*/
+function scrollTo(id){
+    dest = index[id].topDist;
+    if(dest > currentPosition){
+        dir = 'down';
+        scrollDown();
+    }else if(dest < currentPosition){
+        dir = 'up';
+        // pageScroll();
+        scrollUp();
+    }
+    
+}
+/* based on the @dir parameter we get scroll up or down
+
+@dir variable that is valued up/down and indicate if we are scrolling up or down
+*/
+function scrollUp(){
+    if(currentPosition > dest){
+        console.log("currentPosition: "+currentPosition+" dest: "+dest);
+        currentPosition -= 50;    
+        window.scrollBy(0,-50);
+        scrolldelay = setTimeout(scrollUp,10);
+    }else {
+        dest = 0;
+    }
+}
+
+function scrollDown(){
+    if(currentPosition < dest){
+        console.log("currentPosition: "+currentPosition+" dest: "+dest);
+        currentPosition += 50;    
+        window.scrollBy(0,50);
+        scrolldelay = setTimeout(scrollDown,10);
+    }else{
+        dest = 0;
+    }
+}
+
 //navBG function manage the transparency of the navbar   plus update the scrollTop value to be used by the hambuger menu
 const hbMenu = document.querySelector(".mobile-menu");
 const hbMenuLi = document.querySelector('.menu ul');
@@ -18,6 +115,8 @@ function navBG(){
     }else if(scrollTopValue < 100 && !hbIsHidden){
         navbar.classList.remove("scroll");
     }
+
+    updateCurrentPosotion();
 }
 
 // event listener for the action on the navbar
@@ -99,6 +198,7 @@ anime.timeline().add({
 
 
 const ul = document.querySelectorAll("header ul li");
+const logo = document.querySelector("a#home");
 
 ul.forEach(li => {
     li.addEventListener("click", activeLiChange);
@@ -110,22 +210,17 @@ function activeLiChange(){
 
     activeLi.forEach(li => {
         if(!this.isEqualNode(li)){
+            let id = this.parentElement.id;
+            scrollTo(id);
             this.classList.add("active");
             li.classList.remove("active")
         }
     });
 }
 
-function scroll(destIndex) {
-    let elmDistance = getDistanceFromTop(aArray[destIndex]);
-    console.log(elmDistance);
-    toDest = elmDistance - toDest;
-    console.log("toDest: " + toDest);
-    pageScroll();
-}
-
-
-
+logo.addEventListener("click",function(){
+    scrollTo(0);
+});
 
  // portfolio
  $('.gallery ul li a').click(function() {
--- js/scroll.js
+++ js/scroll.js
@@ -1,156 +1,98 @@
-class scrollOjb {
-    /* constructor inizialize the attributes
-    @currentPosition depending on the position where the page gets loaded we get a pointer where we are at
-    @index array containing the amout of pixel from the document top
+/* constructor inizialize the attributes
+@currentPosition depending on the position where the page gets loaded we get a pointer where we are at
+@index array containing the amout of pixel from the document top
+*/
+let currentPosition = document.documentElement.scrollTop;
+let index = scrollPointArray(pointer);
+let pointerIndex = "placeholder";
+let dest = 0;
+let dir = '';
 
-    */
-    constructor(pointer){
-        this.currentPosition = document.documentElement.scrollTop;
-        this.index = this.scrollPointArray(pointer);
-        this.pointerIndex = "placeholder";
-        this.dest = 0;
-        this.dir = '';
-        console.log(this.index);
-    }
-
-    /* array builder for the index
-    @scrollPoints nodeList containing the pointer in the document
-    @scrollPointsIndex array containing the offest from document top for each pointer
-
-    this fucntion return an oject containing:
-    i: name of the destination link
-    topDist: distance from the top of the document of the link
-
-    its easy access thank to the array organization that find itself in the html
-    */
-    scrollPointArray(selector){
-        let scrollPoints = document.querySelectorAll(selector);
-        let scrollPointsIndex = [];
-        
-        for(let i = 0; i < scrollPoints.length; i++){
-            let point = scrollPoints[i];
-            point.setAttribute('pId',i);
-            let crtIndex = {
-                i: point.getAttribute('name'),
-                topDist: this.getDistanceFromTop(point)
-            };
-            scrollPointsIndex.push(crtIndex);
-        }
-
-        return scrollPointsIndex;
-    }
-
-    /* function to calculate the distance from the top of the document of the @element
-    @element element wher the distance get calculated
-    @yPost var containing the final distance ( starts at -100 because the navbar is 100px )
-    */
-    getDistanceFromTop(element) {
-        var yPos = -100;
+/* array builder for the index
+@scrollPoints nodeList containing the pointer in the document
+@scrollPointsIndex array containing the offest from document top for each pointer
+this fucntion return an oject containing:
+i: name of the destination link
+topDist: distance from the top of the document of the link
+its easy access thank to the array organization that find itself in the html
+*/
+function scrollPointArray(selector){
+    let scrollPoints = document.querySelectorAll(selector);
+    let scrollPointsIndex = [];
     
-        while(element) {
-            yPos += (element.offsetTop);
-            element = element.offsetParent;
-        }
-    
-        return yPos;
+    for(let i = 0; i < scrollPoints.length; i++){
+        let point = scrollPoints[i];
+        point.setAttribute('pId',i);
+        let crtIndex = {
+            i: point.getAttribute('name'),
+            topDist: getDistanceFromTop(point)
+        };
+        scrollPointsIndex.push(crtIndex);
     }
-
-    /* update the value of the attribute currentPosition
-    this method can be attached to a event listener on the scroll event to keep track of where we are in the page
-    */
-    updateCurrentPosotion(){
-        this.currentPosition = document.documentElement.scrollTop;
-    }
-    
-    /* select and manage the scroll direction calling the pageScroll method
-    
-    @id is the index id to get the element to scroll to
-    @dest populate the dest variable to indicate where we have to stop
-    */
-    scrollTo(id){
-        this.dest = this.index[id].topDist;
-        if(this.dest > this.currentPosition){
-            this.dir = 'down';
-            setTimeout(this.pageScroll(),1000);
-        }else if(this.dest < this.currentPosition){
-            this.dir = 'up';
-            setTimeout(this.pageScroll(),1000);
-        }
-        this.dest = 0;
-    }
-
-    /* based on the @dir parameter we get scroll up or down
-    
-    @dir variable that is valued up/down and indicate if we are scrolling up or down
-    */
-    pageScroll() { 
-        if(this.dir == 'up'){
-
-            while(this.currentPosition > this.dest){
-                console.log("currentPosition: "+this.currentPosition+" dest: "+this.dest);
-                this.currentPosition -= 50;
-                setTimeout(function(){
-                    window.scrollBy(0,-50);
-                },10);
-            }
-
-        }else if(this.dir == 'down') {
-
-                while(this.currentPosition < this.dest){
-                    console.log("currentPosition: "+this.currentPosition+" dest: "+this.dest);
-                    this.currentPosition += 50;
-                    setTimeout(function(){
-                        window.scrollBy(0,50);
-                    },10);
-                }
-
-        }
-    }
-    
+    return scrollPointsIndex;
 }
 
-let a = new scrollOjb("#scroll-point");
+/* function to calculate the distance from the top of the document of the @element
+@element element wher the distance get calculated
+@yPost var containing the final distance ( starts at -100 because the navbar is 100px )
+*/
+function getDistanceFromTop(element) {
+    let yPos = -100;
 
-document.addEventListener("scroll",function(){
-    a.updateCurrentPosotion();
-});
+    while(element) {
+        yPos += (element.offsetTop);
+        element = element.offsetParent;
+    }
 
-// console.log(a.updateCurrentPosotion());
+    return yPos;
+}
+/* update the value of the attribute currentPosition
+this method can be attached to a event listener on the scroll event to keep track of where we are in the page
+*/
+function updateCurrentPosotion(){
+    currentPosition = document.documentElement.scrollTop;
+}
 
-//     let clickedDest = document.querySelector("a li.active").parentElement;
-//     scroll(clickedDest.getAttribute("id"));
+/* select and manage the scroll direction calling the pageScroll method
 
-//     function getDistanceFromTop(element) {
-//         var yPos = -100;
-    
-//         while(element) {
-//             yPos += (element.offsetTop);
-//             element = element.offsetParent;
-//         }
-    
-//         return yPos;
-//     }
-    
-//     function pageScroll() {
-//         if(currentPosition < toDest){
-//             window.scrollBy(0,50);
-//             currentPosition += 50;
-//             scrolldelay = setTimeout(pageScroll,10);
-//         }
-//         // else {
-//         //     toDest = currentPosition = 0;
-//         // }
-//     }
+@id is the index id to get the element to scroll to
+@dest populate the dest variable to indicate where we have to stop
+*/
+function scrollTo(id){
+    dest = index[id].topDist;
+    if(dest > currentPosition){
+        dir = 'down';
+        scrollDown();
+    }else if(dest < currentPosition){
+        dir = 'up';
+        // pageScroll();
+        scrollUp();
+    }
+    dest = 0;
+}
+/* based on the @dir parameter we get scroll up or down
 
-//     let toDest = 0, currentPosition = 0;
+@dir variable that is valued up/down and indicate if we are scrolling up or down
+*/
+function scrollUp(){
+    if(currentPosition > dest){
+        console.log("currentPosition: "+currentPosition+" dest: "+dest);
+        currentPosition -= 50;    
+        window.scrollBy(0,-50);
+        let x = setTimeout(scrollUp(currentPosition, dest),10);
+    }else {
+        console.log("exit currentPosition: "+currentPosition+" dest: "+dest);
+    }
+}
 
-// //variable and code to save the link to navigate in the page
-// let a = document.querySelectorAll("a");
-// let aArray = [];
+function scrollDown(){
+    if(currentPosition < dest){
+        console.log("currentPosition: "+currentPosition+" dest: "+dest);
+        currentPosition += 50;    
+        window.scrollBy(0,50);
+        let x = setTimeout(scrollDown(currentPosition, dest),10);
+    }else{
+        console.log("exit currentPosition: "+currentPosition+" dest: "+dest);
+    }
+}
 
-// a.forEach(aElement => {
-//     let currentName = aElement.getAttribute("name");
-//     if(isNaN(currentName)){
-//        aArray.push(aElement);
-//     }
-// })
